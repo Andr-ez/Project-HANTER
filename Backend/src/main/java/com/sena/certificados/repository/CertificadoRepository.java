6682a0eed6787b8551f3a6132de9ff61731ee8d8
jaimote123 <jaimemarin32261@gmail.com>
2026-02-17 13:33:23 -0500

    Delete Backend directory

--- Backend/src/main/java/com/sena/certificados/repository/CertificadoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sena.certificados.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sena.certificados.model.Certificado;
-
-public interface CertificadoRepository extends JpaRepository<Certificado, Integer> {
-    // Aquí puedes agregar consultas personalizadas si las necesitas más adelante
-}
