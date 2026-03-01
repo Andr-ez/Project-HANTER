e8eb462e107b1da232454c7edc4c26eb090eb8a6
jaimote123 <jaimemarin32261@gmail.com>
2026-02-28 23:00:40 -0500

    Refactor JSX structure in 000.jsx

--- Frontend/src/000/000.jsx
+++ Frontend/src/000/000.jsx
@@ -13,40 +13,39 @@ function Bienvenido() {
   return (
   <div className="bienvenido-page">
     <div>
-
+     
       {/* Círculo de fondo */}
-      <div className="circuloFondo"></div>
+      <div className="circuloFondo">
 
+        {/* Logo */}
+        <div className="logo-container">
+          <img src="/fotos/LOGO MEL.png" alt="Logo" />
+        </div>
 
-      {/* Logo */}
-      <div className="logo-container">
-        <img src="/fotos/LOGO MEL.png" alt="Logo" />
-      </div>
-
-
-      {/* Título y botones principales */}
-      <div className="title">
-        <h1>¡BIENVENIDO!</h1>
-
+        {/* Título y botones principales */}
+        <div className="title">
+         <h1>¡BIENVENIDO!</h1>
+        </div>
+        
         <div className="buttons">
           <Link to="/001" className="btnInicioSesion">INICIAR SESIÓN</Link>
           <Link to="/004" className="btnRegistrarse">REGISTRARSE</Link>
         </div>
-      </div>
+      
 
 
-      {/* Botones inferiores */}
-      <Link to="/003" className="btnDudas">?</Link>
-      <Link to="/004" className="btnReporte">!</Link>
+        {/* Botones inferiores */}
+        <Link to="/003" className="btnDudas">?</Link>
+        <Link to="/004" className="btnReporte">!</Link>
 
+      
 
-      {/* Círculos decorativos */}
-      <div className="circulosDecorativos">
-        <div className="circulo1"></div>
-        <div className="circulo2"></div>
-        <div className="circulo3"></div>
-      </div>
+        {/* circulos decorativos*/}
+          <div className="cGDecor-1"></div>
+          <div className="cGDecor-2"></div>
+          <div className="cGDecor-3"></div>
 
+</div>
     </div>
   </div>
   );
